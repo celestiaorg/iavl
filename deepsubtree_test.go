9f72ee60a2d7832f720b6b74624cd82bd0d0e9b8
Manav Aggarwal <manavaggarwal1234@gmail.com>
2022-10-16 09:00:25 -0400

    Refactor test code

--- deepsubtree_test.go
+++ deepsubtree_test.go
@@ -59,61 +59,57 @@ func TestDeepSubtreeVerifyProof(t *testing.T) {
 // correct roots
 // Reference: https://ethresear.ch/t/data-availability-proof-friendly-state-tree-transitions/1453/23
 func TestDeepSubtree(t *testing.T) {
-	tree, err := getTestTree(5)
-	require.NoError(t, err)
 	require := require.New(t)
+	getTree := func() *MutableTree {
+		tree, err := getTestTree(5)
+		require.NoError(err)
 
-	tree.Set([]byte("e"), []byte{5})
-	tree.Set([]byte("d"), []byte{4})
-	tree.Set([]byte("c"), []byte{3})
-	tree.Set([]byte("b"), []byte{2})
-	tree.Set([]byte("a"), []byte{1})
+		tree.Set([]byte("e"), []byte{5})
+		tree.Set([]byte("d"), []byte{4})
+		tree.Set([]byte("c"), []byte{3})
+		tree.Set([]byte("b"), []byte{2})
+		tree.Set([]byte("a"), []byte{1})
 
-	rootHash, _, err := tree.SaveVersion()
-	require.NoError(err)
+		_, _, err = tree.SaveVersion()
+		require.NoError(err)
+		return tree
+	}
 
-	fmt.Println("PRINT TREE")
-	_ = printNode(tree.ndb, tree.root, 0)
-	fmt.Println("PRINT TREE END")
+	testCases := [][][]byte{
+		{
+			[]byte("a"), []byte("b"),
+		},
+		{
+			[]byte("c"), []byte("d"),
+		},
+	}
 
-	mutableTree, err := NewMutableTree(db.NewMemDB(), 100, false)
-	require.NoError(err)
-	dst := DeepSubTree{mutableTree}
+	for _, subsetKeys := range testCases {
+		tree := getTree()
+		rootHash := tree.root.hash
+		mutableTree, err := NewMutableTree(db.NewMemDB(), 100, false)
+		require.NoError(err)
+		dst := DeepSubTree{mutableTree}
+		for _, subsetKey := range subsetKeys {
+			//proof, _, _, err := tree.getRangeProof([]byte("a"), nil, 1)
+			err = dst.AddPath(tree.ImmutableTree, subsetKey)
+			require.NoError(err)
+			dst.BuildTree(rootHash)
+			require.NoError(err)
+		}
+		dst.SaveVersion()
 
-	//proof, _, _, err := tree.getRangeProof([]byte("a"), nil, 1)
-	err = dst.AddPath(tree.ImmutableTree, []byte("a"))
-	require.NoError(err)
-	dst.BuildTree(rootHash)
-	require.NoError(err)
+		// Check root hashes are equal
+		require.Equal(dst.root.hash, tree.root.hash)
 
-	//proof, _, _, err = tree.getRangeProof([]byte("b"), nil, 1)
-	err = dst.AddPath(tree.ImmutableTree, []byte("b"))
-	require.NoError(err)
-	err = dst.BuildTree(rootHash)
-	require.NoError(err)
-	dst.SaveVersion()
+		values := [][]byte{{10}, {20}}
+		for i, subsetKey := range subsetKeys {
+			dst.Set(subsetKey, values[i])
+			dst.SaveVersion()
+			tree.Set(subsetKey, values[i])
+			tree.SaveVersion()
+		}
 
-	fmt.Println("PRINT DST TREE")
-	_ = dst.printNodeDeepSubtree(dst.ImmutableTree.root, 0)
-	fmt.Println("PRINT DST TREE END")
-	fmt.Println()
-
-	// Check root hashes are equal
-	require.Equal(dst.root.hash, tree.root.hash)
-
-	dst.Set([]byte("a"), []byte{10})
-	dst.SaveVersion()
-	tree.Set([]byte("a"), []byte{10})
-	tree.SaveVersion()
-	dst.Set([]byte("b"), []byte{20})
-	dst.SaveVersion()
-	tree.Set([]byte("b"), []byte{20})
-	tree.SaveVersion()
-
-	fmt.Println("PRINT DST TREE")
-	_ = dst.printNodeDeepSubtree(dst.ImmutableTree.root, 0)
-	fmt.Println("PRINT DST TREE END")
-	fmt.Println()
-
-	require.Equal(dst.root.hash, tree.root.hash)
+		require.Equal(dst.root.hash, tree.root.hash)
+	}
 }

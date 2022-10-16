79040307a7b3d1c7acc3462356612edd304246f5
Manav Aggarwal <manavaggarwal1234@gmail.com>
2022-10-15 23:27:31 -0400

    Fix deepsubtree, all checks pass

--- deepsubtree.go
+++ deepsubtree.go
@@ -1,6 +1,7 @@
 package iavl
 
 import (
+	"bytes"
 	"fmt"
 )
 
@@ -28,14 +29,8 @@ func (dst *DeepSubTree) addPath(pl PathToLeaf, leaf *Node) error {
 		return err
 	}
 
-	n := &Node{
-		subtreeHeight: 0,
-		size:          1,
-		version:       leaf.version,
-		key:           leaf.key,
-		value:         leaf.value,
-		hash:          leaf.hash,
-	}
+	n := NewNode(leaf.key, leaf.value, leaf.version)
+	n._hash()
 	err = dst.ndb.SaveNode(n)
 	if err != nil {
 		return err
@@ -59,11 +54,9 @@ func (dst *DeepSubTree) addPath(pl PathToLeaf, leaf *Node) error {
 			version:       pin.Version,
 			leftHash:      pin.Left,
 			rightHash:     pin.Right,
+			hash:          hash,
 		}
-		prevHash, err = n._hash()
-		if err != nil {
-			return err
-		}
+		prevHash = hash
 
 		has, err := dst.ndb.Has(n.hash)
 		if err != nil {
@@ -76,8 +69,7 @@ func (dst *DeepSubTree) addPath(pl PathToLeaf, leaf *Node) error {
 			}
 		}
 		if i == 0 {
-			_, _, err = dst.SaveVersion()
-			if err != nil {
+			if err := dst.ndb.Commit(); err != nil {
 				return err
 			}
 		}
@@ -99,15 +91,13 @@ func (dst *DeepSubTree) BuildTree(rootHash []byte) error {
 		if len(pnode.rightHash) > 0 && pnode.rightNode == nil {
 			pnode.rightNode, _ = dst.ndb.GetNode(pnode.rightHash)
 		}
-		/*
-			if pnode.leftNode != nil {
-				pnode.key = pnode.leftNode.getHighestKey()
-			}
+		if pnode.leftNode != nil {
+			pnode.key = pnode.leftNode.getHighestKey()
+		}
 
-			if pnode.rightNode != nil {
-				pnode.key = pnode.rightNode.getLowestKey()
-			}
-		*/
+		if pnode.rightNode != nil {
+			pnode.key = pnode.rightNode.getLowestKey()
+		}
 	}
 	if dst.root == nil {
 		rootNode, rootErr := dst.ndb.GetNode(rootHash)
@@ -118,6 +108,70 @@ func (dst *DeepSubTree) BuildTree(rootHash []byte) error {
 	}
 
 	return nil
+}
+
+func (dst *DeepSubTree) Set(key []byte, value []byte) (updated bool, err error) {
+	if value == nil {
+		return updated, fmt.Errorf("attempt to store nil value at key '%s'", key)
+	}
+
+	dst.root, updated, err = dst.recursiveSet(dst.root, key, value)
+	dst.root.hash = nil
+	dst.root._hash()
+	return updated, err
+}
+
+func (dst *DeepSubTree) recursiveSet(node *Node, key []byte, value []byte) (
+	newSelf *Node, updated bool, err error,
+) {
+	version := dst.version + 1
+
+	if node.isLeaf() {
+		switch bytes.Compare(key, node.key) {
+		case -1:
+			return &Node{
+				key:           node.key,
+				subtreeHeight: 1,
+				size:          2,
+				leftNode:      NewNode(key, value, version),
+				rightNode:     node,
+				version:       version,
+			}, false, nil
+		case 1:
+			return &Node{
+				key:           key,
+				subtreeHeight: 1,
+				size:          2,
+				leftNode:      node,
+				rightNode:     NewNode(key, value, version),
+				version:       version,
+			}, false, nil
+		default:
+			return NewNode(key, value, version), true, nil
+		}
+	} else {
+		node.version = version
+		if bytes.Compare(key, node.key) < 0 || node.rightNode == nil {
+			leftNode := node.leftNode
+			node.leftNode, updated, err = dst.recursiveSet(leftNode, key, value)
+			if err != nil {
+				return nil, updated, err
+			}
+			node.leftNode.hash = nil
+			node.leftNode._hash()
+			node.leftHash = node.leftNode.hash
+		} else {
+			rightNode := node.rightNode
+			node.rightNode, updated, err = dst.recursiveSet(rightNode, key, value)
+			if err != nil {
+				return nil, updated, err
+			}
+			node.rightNode.hash = nil
+			node.rightNode._hash()
+			node.rightHash = node.rightNode.hash
+		}
+		return node, updated, nil
+	}
 }
 
 func (dst *DeepSubTree) printNodeDeepSubtree(node *Node, indent int) error {
@@ -148,4 +202,43 @@ func (dst *DeepSubTree) printNodeDeepSubtree(node *Node, indent int) error {
 		dst.printNodeDeepSubtree(node.leftNode, indent+1)
 	}
 	return nil
+}
+
+// Returns the highest key in the node's subtree
+func (node *Node) getHighestKey() []byte {
+	if node.isLeaf() {
+		return node.key
+	}
+	highestKey := []byte{}
+	if node.rightNode != nil {
+		highestKey = node.rightNode.getHighestKey()
+	}
+	if node.leftNode != nil {
+		leftHighestKey := node.leftNode.getHighestKey()
+		if len(highestKey) == 0 {
+			highestKey = leftHighestKey
+		} else if string(leftHighestKey) > string(highestKey) {
+			highestKey = leftHighestKey
+		}
+	}
+	return highestKey
+}
+
+func (node *Node) getLowestKey() []byte {
+	if node.isLeaf() {
+		return node.key
+	}
+	lowestKey := []byte{}
+	if node.rightNode != nil {
+		lowestKey = node.rightNode.getLowestKey()
+	}
+	if node.leftNode != nil {
+		leftLowestKey := node.leftNode.getLowestKey()
+		if len(lowestKey) == 0 {
+			lowestKey = leftLowestKey
+		} else if string(leftLowestKey) < string(lowestKey) {
+			lowestKey = leftLowestKey
+		}
+	}
+	return lowestKey
 }

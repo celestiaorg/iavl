269fd08f4b0bf22bac63e01f0b1f1828b2f62195
Manav Aggarwal <manavaggarwal1234@gmail.com>
2022-10-19 23:41:07 -0400

    Add test to check add operation in deepsubtree: WIP

--- deepsubtree_test.go
+++ deepsubtree_test.go
@@ -113,3 +113,63 @@ func TestDeepSubtreeWithUpdates(t *testing.T) {
 		require.Equal(dst.root.hash, tree.root.hash)
 	}
 }
+
+// Tests adding and deleting keys in the deepsubtree returns the
+// correct roots
+func TestDeepSubtreeWWithAddsAndDeletes(t *testing.T) {
+	require := require.New(t)
+	getTree := func() *MutableTree {
+		tree, err := getTestTree(5)
+		require.NoError(err)
+
+		tree.Set([]byte("b"), []byte{2})
+		tree.Set([]byte("a"), []byte{1})
+
+		_, _, err = tree.SaveVersion()
+		require.NoError(err)
+		return tree
+	}
+	tree := getTree()
+	fmt.Println("PRINT TREE")
+	_ = printNode(tree.ndb, tree.root, 0)
+	fmt.Println("PRINT TREE END")
+
+	subsetKeys := [][]byte{
+		[]byte("b"),
+	}
+	rootHash := tree.root.hash
+	mutableTree, err := NewMutableTree(db.NewMemDB(), 100, false)
+	require.NoError(err)
+	dst := DeepSubTree{mutableTree}
+	for _, subsetKey := range subsetKeys {
+		err = dst.AddPath(tree.ImmutableTree, subsetKey)
+		require.NoError(err)
+		dst.BuildTree(rootHash)
+		require.NoError(err)
+	}
+	dst.SaveVersion()
+
+	fmt.Println("PRINT DST TREE")
+	_ = dst.printNodeDeepSubtree(dst.ImmutableTree.root, 0)
+	fmt.Println("PRINT DST TREE END")
+
+	// Check root hashes are equal
+	require.Equal(dst.root.hash, tree.root.hash)
+
+	keyToAdd := []byte("c")
+	valueToAdd := []byte{3}
+	// Add a key, c, to the tree and the dst
+	dst.Set(keyToAdd, valueToAdd)
+	dst.SaveVersion()
+	fmt.Println("PRINT DST TREE")
+	_ = dst.printNodeDeepSubtree(dst.ImmutableTree.root, 0)
+	fmt.Println("PRINT DST TREE END")
+	tree.Set(keyToAdd, valueToAdd)
+	tree.SaveVersion()
+	fmt.Println("PRINT TREE")
+	_ = printNode(tree.ndb, tree.root, 0)
+	fmt.Println("PRINT TREE END")
+
+	// Check root hashes are equal
+	require.Equal(dst.root.hash, tree.root.hash)
+}
